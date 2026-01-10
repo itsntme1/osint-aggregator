76e9473fad058caf5ce203a9038558d3c30a8fe5
itsntme1 <itisntme12@protonmail.com>
2026-01-10 18:12:57 +0100

    feat: added a gitignore

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 
 #import sherlock
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+import functions as f
+from secret_keys import session_key
 
 app = Flask(__name__)
-
+app.secret_key = session_key
 
 @app.route("/", methods = ["GET", "POST"])
 def landing():
 
     if request.method == "POST":
+        session["search_items"] = f.format_search_input(request.form.get("search_input"))
+
         return redirect("/search")
 
     return render_template("landing.html")
 
 @app.route("/search")
 def search():
-    return render_template("search.html")
+    search_items = session["search_items"]
+    print(search_items)
+    return render_template("search.html", search_items=search_items)
 
 
 
