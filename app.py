99292277c7ff883d21b7bfb845827ce51ab0d4c0
itsntme1 <itisntme12@protonmail.com>
2026-01-13 23:50:35 +0100

    bug: removed leaked api key

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 
-#import sherlock
 from flask import Flask, render_template, request, redirect, session
-import functions as f
-from secret_keys import session_key
+from functions import *
+from secret_keys import *
 
 app = Flask(__name__)
 app.secret_key = session_key
@@ -11,7 +10,7 @@ app.secret_key = session_key
 def landing():
 
     if request.method == "POST":
-        session["search_items"] = f.format_search_input(request.form.get("search_input"))
+        session["search_items"] = format_search_input(request.form.get("search_input"))
 
         return redirect("/search")
 
@@ -19,9 +18,18 @@ def landing():
 
 @app.route("/search")
 def search():
+#    ip_info = f.query_ip_info(request.remote_addr)
 
     return render_template("search.html", search_items=session["search_items"])
 
+@app.route("/api/ip_info")
+def ip_info():
+    # data = query_ip_info(request.remote_addr)
+    data = query_ip_info("90.177.145.18", ip_info_key)
+
+    print(data)
+
+    return data
 
 
 if __name__ == "__main__":
--- functions.py
+++ functions.py
@@ -1,16 +1,38 @@
-import re
+import re, requests
 
 def format_search_input(search_input: str):
     input_items = search_input.strip().split(' ')
     
-    # [[usernames], [emails]]
-    search_items = [[],[]]
+    search_items = {
+        'usernames': [],
+        'emails': []
+    }
 
     for item in input_items:
-        if re.search("\w+@\w+\.\w{2,}", item):
-            search_items[1].append(item)
+        if re.search(r"\w+@\w+\.\w{2,}", item):
+            search_items["emails"].append(item)
 
         else:
-            search_items[0].append(item)
+            search_items["usernames"].append(item)
 
     return search_items
+
+def query_api(endpoint: str, headers, timeout: int=1):
+    response = None
+
+    try:
+        response = requests.get(endpoint, headers=headers, timeout=timeout)
+
+        if response.status_code != 200:
+            print(f"Status code: {response.status_code}")
+            response = None
+
+            return
+
+    except Exception as error:
+        print(f"query_api: {error}")
+    
+    return response
+
+def query_ip_info(ip, token):
+    return query_api(f"https://ipinfo.io/{ip}", {"Authorization": f"Bearer {token}"}).json()
