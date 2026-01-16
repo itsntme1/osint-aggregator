6349c6fe9ef9c8ea63f9a03797143d58f9ddd3e2
itsntme1 <itisntme12@protonmail.com>
2026-01-16 11:33:26 +0100

    feat: card macro, simplified js

--- static/script.js
+++ static/script.js
@@ -1,6 +1,9 @@
 
-async function loadData(element, endpoint, format) {
+async function loadData(element, format) {
     try {
+        const endpoint = element.closest("[data-endpoint]").dataset.endpoint
+        console.log(endpoint)
+
         const response = await fetch(endpoint);
         const data = await response.json();
 
@@ -10,25 +13,25 @@ async function loadData(element, endpoint, format) {
         element.innerHTML = `<img src="/static/warning-icon.png"><br>Failed to load`;
     }
 }
-function reloadData(element, endpoint, format) {
-    element = element.closest(".data-div").querySelector(".data-display");
+function reloadData(element, format) {
+    element = element.closest("[name]").querySelector(".[data-display]");
 
     loadData(element, endpoint, format);
 }
 
-function assignLoadData() {
-    document.querySelectorAll(".data-display").forEach(element => {
-        loadData(element, "/api/ip_info", d => d.ip);
+function initialLoadData() {
+    document.querySelectorAll("[data-display]").forEach(element => {
+        loadData(element, d => d.ip);
     });
 }
 
 function assignReloadData() {
-    document.querySelectorAll(".data-reload").forEach(element => {
+    document.querySelectorAll("[data-reload]").forEach(element => {
         element.addEventListener("click", () => {
-            reloadData(element, "/api/ip_info", d => d.ip);
+            reloadData(element, d => d.ip);
         });
     });
 }
 
-window.addEventListener("DOMContentLoaded", assignLoadData);
+window.addEventListener("DOMContentLoaded", initialLoadData);
 window.addEventListener("DOMContentLoaded", assignReloadData);
